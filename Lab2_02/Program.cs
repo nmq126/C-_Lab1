c68d99f6d3f6e832617666e8f4c6b6abce9de196
nmq126 <minhquangngo.98@gmail.com>
2021-09-24 21:05:04 +0700

    2nd commit lab 2

---
+++ Lab2_02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lab2_02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ObjectArray array = new ObjectArray();
+            array.Run();
+            Console.ReadLine();
+        }
+
+        class Employee
+        {
+            private int empID;
+
+            public Employee(int empID)
+            {
+                this.empID = empID;
+            }
+
+            public override string ToString()
+            {
+                return empID.ToString();
+            }
+        }
+
+        class ObjectArray
+        {
+            public void Run()
+            {
+                int[] intArray;
+                Employee[] empArray;
+                intArray = new int[5];
+                empArray = new Employee[3];
+                for (int i = 0; i < empArray.Length; i++)
+                {
+                    empArray[i] = new Employee(i + 5);
+
+                }
+
+                Console.WriteLine("The int array...");
+                for (int i = 0; i < intArray.Length; i++)
+                {
+                    Console.WriteLine(intArray[i].ToString());
+                }
+
+                Console.WriteLine("\nThe employee array...");
+                for (int i = 0; i < empArray.Length; i++)
+                {
+                    Console.WriteLine(empArray[i].ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Lab2_ex1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab2_ex1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int sin;
+            while (true)
+            {
+                Console.WriteLine("Enter your SIN:");
+                sin = Convert.ToInt32(Console.ReadLine());
+                if (sin == 0)
+                {
+                    break;
+                }
+                if (CheckValid(sin))
+                {
+                    Console.WriteLine("This is a valid SIN");
+                }
+                else
+                {
+                    Console.WriteLine("This is an invalid SIN");
+                }
+            }
+            Console.WriteLine("Have a nice day!!!");
+        }
+
+        public static bool CheckValid(int sinNumber)
+        {
+            if (sinNumber < 100000000 || sinNumber > 999999999)
+            {
+                return false;
+            }
+
+            char[] charArray = sinNumber.ToString().ToCharArray();
+            int[] intArray = Array.ConvertAll(charArray, c => (int) Char.GetNumericValue(c));
+            int firstSum = CalculateEvenPositionDigits(intArray[1]) + 
+                           CalculateEvenPositionDigits(intArray[3]) +
+                           CalculateEvenPositionDigits(intArray[5]) + 
+                           CalculateEvenPositionDigits(intArray[7]);
+            int secondSum = intArray[0] + intArray[2] + intArray[4] + intArray[6];
+            int total = firstSum + secondSum;
+            int nextHighestNumberEndingInZero = ((total / 10) + 1) * 10;
+            int finalNumber = nextHighestNumberEndingInZero - total;
+            return (finalNumber.Equals(intArray[8]));
+        }
+
+        public static int CalculateEvenPositionDigits(int a)
+        {
+            a = a * 2;
+            if (a < 10)
+            {
+                return a;
+            }
+            else
+            {
+                return a - 10 + 1;
+            }
+        }
+    }
+}
